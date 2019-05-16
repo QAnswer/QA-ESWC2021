615d18be181d31a0120929e168747f8ec517d8d7
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2019-05-15 17:28:40 -0700

    Fixed some links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1', '')}>
+            <a href={this.docUrl('doc0', '')}>
               Getting Started
             </a>
             <a href={this.docUrl('api1', '')}>
